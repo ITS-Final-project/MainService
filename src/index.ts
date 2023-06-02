3ff8df8488ed3d2840c46670d57d43e72f7b4f83
Andrej Tasevski Mestrovic <akitasevski112@gmail.com>
2023-06-02 20:32:17 +0200

    Simple Http server setup

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+    
+    res.render('index.ejs', { title: 'Express' });
+
+    }
+);
+
+app.get('/classificator', (req, res) => {
+        
+    res.render('classificator.ejs', { title: 'Express' });
+
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    }
+);
