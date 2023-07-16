26e4f637142479bda4bd055f28c8a8cac1bb245c
Andrej Tasevski Mestrovic <akitasevski112@gmail.com>
2023-07-17 01:14:58 +0200

    Updated animations

---
+++ src/public/js/error_page.js
@@ -0,0 +1,6 @@
+function fadeOut(){
+    var errorContainer = document.getElementById("error-container");
+
+    // Add css animation
+    errorContainer.style.animation = "fadeOut 0.5s ease-in-out forwards";
+}
